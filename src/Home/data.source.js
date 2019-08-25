18620e742186a01d964755cad9164dc08f0f84a8
GJW2016 <837165827@qq.com>
2019-08-26 00:28:03 +0800

    new

---
+++ src/Home/data.source.js
@@ -0,0 +1,177 @@
+import React from 'react';
+export const Nav00DataSource = {
+  wrapper: { className: 'header0 home-page-wrapper' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header0-logo',
+    children: 'https://os.alipayobjects.com/rmsportal/mlcYmsRilwraoAe.svg',
+  },
+  Menu: {
+    className: 'header0-menu',
+    children: [
+      {
+        name: 'item0',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [{ children: 'Cultural Info', name: 'text' }],
+        },
+        subItem: [
+          {
+            name: 'sub0',
+            className: 'item-sub',
+            children: {
+              href: '/page2',
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image',
+                  children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg',
+                },
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: 'China',
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: 'China',
+                },
+              ],
+            },
+          },
+          {
+            name: 'sub1',
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image',
+                  children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg',
+                },
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: 'India',
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: 'India',
+                },
+              ],
+            },
+          },
+        ],
+      },
+      {
+        name: 'item1',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [{ children: 'Data analysis', name: 'text' }],
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header0-mobile-menu' },
+};
+export const Banner01DataSource = {
+  wrapper: { className: 'banner0' },
+  textWrapper: { className: 'banner0-text-wrapper' },
+  title: {
+    className: 'banner0-title',
+    children: 'https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png',
+  },
+  content: {
+    className: 'banner0-content',
+    children: (
+      <>
+        <p>Get to know your life</p>
+      </>
+    ),
+  },
+  button: { className: 'banner0-button', children: (<a href="https://motion.ant.design">learn more</a>), },
+};
+export const Footer10DataSource = {
+  wrapper: { className: 'home-page-wrapper footer1-wrapper' },
+  OverPack: { className: 'footer1', playScale: 0.2 },
+  block: {
+    className: 'home-page',
+    gutter: 0,
+    children: [
+      {
+        name: 'block0',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: {
+          className: 'logo',
+          children:
+            'https://zos.alipayobjects.com/rmsportal/qqaimmXZVSwAhpL.svg',
+        },
+        childWrapper: {
+          className: 'slogan',
+          children: [
+            {
+              name: 'content0',
+              children: (
+                <>
+                  <p>
+                    <br />
+                  </p>
+                </>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: {
+          children: (
+            <>
+              <p>About us</p>
+            </>
+          ),
+        },
+        childWrapper: {
+          children: [
+            { href: '#', name: 'link0', children: 'FAQ' },
+            {
+              href: '#',
+              name: 'link1',
+              children: (
+                <>
+                  <p>Contact us</p>
+                </>
+              ),
+            },
+          ],
+        },
+      },
+    ],
+  },
+  copyrightWrapper: { className: 'copyright-wrapper' },
+  copyrightPage: { className: 'home-page' },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <>
+        <span>
+          ©2019 by <a href="https://motion.ant.design">B16</a> All Rights
+          Reserved
+        </span>
+      </>
+    ),
+  },
+};
---
+++ src/Selection/data.source.js
@@ -0,0 +1,135 @@
+import React from 'react';
+export const Nav00DataSource = {
+  wrapper: { className: 'header0 home-page-wrapper' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header0-logo',
+    children: 'https://os.alipayobjects.com/rmsportal/mlcYmsRilwraoAe.svg',
+  },
+  Menu: {
+    className: 'header0-menu',
+    children: [
+      {
+        name: 'item0',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [{ children: '导航一', name: 'text' }],
+        },
+        subItem: [
+          {
+            name: 'sub0',
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image',
+                  children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg',
+                },
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: 'Ant Design',
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: '企业级 UI 设计体系',
+                },
+              ],
+            },
+          },
+          {
+            name: 'sub1',
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image',
+                  children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg',
+                },
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: 'Ant Design',
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: '企业级 UI 设计体系',
+                },
+              ],
+            },
+          },
+        ],
+      },
+      {
+        name: 'item1',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [{ children: '导航二', name: 'text' }],
+        },
+      },
+      {
+        name: 'item2',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [{ children: '导航三', name: 'text' }],
+        },
+      },
+      {
+        name: 'item3',
+        className: 'header0-item',
+        children: {
+          href: '#',
+          children: [{ children: '导航四', name: 'text' }],
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header0-mobile-menu' },
+};
+export const Feature20DataSource = {
+  wrapper: { className: 'home-page-wrapper content2-wrapper' },
+  OverPack: { className: 'home-page content2', playScale: 0.3 },
+  imgWrapper: { className: 'content2-img', md: 10, xs: 24 },
+  img: {
+    children: 'https://zos.alipayobjects.com/rmsportal/tvQTfCupGUFKSfQ.png',
+  },
+  textWrapper: { className: 'content2-text', md: 14, xs: 24 },
+  title: {
+    className: 'content2-title',
+    children: (
+      <>
+        <p>
+          <b>China</b>
+        </p>
+      </>
+    ),
+  },
+  content: {
+    className: 'content2-content',
+    children: (
+      <>
+        <p>
+          <b>
+            China (Chinese: 中国; pinyin: Zhōngguó; lit. "Middle Kingdom" or
+            "Central State"), officially the People's Republic of China (PRC),
+            is a country in East Asia and the world's most populous country,
+            with a population of around 1.404 billion.[10] Covering
+            approximately 9,600,000 square kilometers (3,700,000 sq mi), it is
+            the third or fourth largest country by total area.
+          </b>
+        </p>
+      </>
+    ),
+  },
+};
