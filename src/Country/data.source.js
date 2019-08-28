49964edbf7b6f8782e334590a8bf22edbd34bba0
GJW2016 <837165827@qq.com>
2019-08-28 14:46:18 +0800

    iteration1--version0.9

---
+++ src/Country/data.source.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+export const Pricing00DataSource = {
+  wrapper: { className: 'home-page-wrapper pricing0-wrapper' },
+  OverPack: {
+    playScale: 0.3,
+    className: 'home-page pricing0 jzsf9rag41k-editor_css',
+    gutter: 0,
+  },
+  imgWrapper: {
+    className: 'pricing0-img-wrapper jzsfg1nsydh-editor_css',
+    md: 12,
+    xs: 24,
+  },
+  img: {
+    className: 'pricing0-img',
+    name: 'image',
+    children:
+      'https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*OnyWT4Nsxy0AAAAAAAAAAABjARQnAQ',
+  },
+  childWrapper: {
+    className: 'pricing0-text-wrapper',
+    md: 12,
+    xs: 24,
+    children: [
+      {
+        name: 'title',
+        children: (
+          <>
+            <p>
+              <b>Taboo</b>
+            </p>
+          </>
+        ),
+        className: 'pricing0-title',
+      },
+      {
+        name: 'pricing',
+        children: (
+          <>
+            <p>People don't send clock as a gift because it means bad luck.</p>
+          </>
+        ),
+        className: 'pricing0-pricing',
+      },
+{
+        name: 'button',
+        children: {
+          href: '#',
+          type: 'primary',
+          children: 'Previous',
+          className: 'jzsf6tebesu-editor_css',
+          icon: 'https://landing.ant.design',
+        },
+      },
+      {
+        name: 'button~jzsf74hd8qu',
+        className: 'jzsf76x9upj-editor_css',
+        children: {
+          children: 'Next',
+          href: '#',
+          type: 'primary',
+          icon: 'https://blog.csdn.net/C11611/article/details/81878570',
+          className: 'jzsf75i7dcm-editor_css',
+        },
+      },
+    ],
+  },
+};
