3fd67432860176ecf2bacb7cc4351dbf5f17ca25
GJW2016 <837165827@qq.com>
2019-10-05 01:43:17 +0800

    iteration3--foodie update

--- src/Foodie/index.js
+++ src/Foodie/index.js
@@ -15,10 +15,10 @@ class Foodie extends Component{
 
   render() {
     return (
-      <div id="scroll" className="scroll">
+      <div id="scroll" className="home">
         <div onClick={this.nextPage} className="arrow-down">
           <div>keep scrolling to find a way to curb your hunger</div>
-          <Icon type="arrow-down" style={{ }}></Icon>
+          <Icon type="arrow-down" style={{ fontSize: 20, color: "#fff" }}></Icon>
         </div>
         <div className="title1">
           <div className="title1-context">
@@ -32,7 +32,7 @@ class Foodie extends Component{
           </div>
         </div>
         <div className="title1 title2">
-          <div className="title1-context">
+          <div className="title1-context" style={{ left: 30 }}>
             <h3>Are you a Foodie?</h3>
             <p>
               Food is an integral part of people's lives, and is much more than just a means of survival. Coming to Melbourne, one might be easily become overwhelmed by the enormous choices of food.
@@ -40,6 +40,21 @@ class Foodie extends Component{
               you're new in the city and are inquisitive about the authentic Australian cuisine which sometime tends to get lost with the range of varied cuisines that has now become an integral part
               of Melbourne's food culture. Brace yourselves as you are in for a ride that will take you in for a journey of Australian food while embracing your home country's food culture.
             </p>
+          </div>
+        </div>
+        <div className="select">
+          <h3>title</h3>
+          <h2>Make your choice</h2>
+          <div className="flex-block">
+            <div className="slect-item">
+              <h4>Cook at home</h4>
+              <img src="https://www.culturallydiversity.tk/images/iter3/food.jpg" alt=""></img>
+            </div>
+            <span>OR</span>
+            <div className="slect-item">
+              <h4>Find a budget friendly restaurant</h4>
+              <img src="https://www.culturallydiversity.tk/images/iter3/restaurant.jpg" alt=""></img>
+            </div>
           </div>
         </div>
       </div>
